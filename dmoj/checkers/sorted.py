de0560a6d6cfab62a6871b32bfc16e6f99d3ffab
Phoenix <Phoenix1369@users.noreply.github.com>
2017-06-27 14:18:45 -0400

    Add ignorance of order in multiple ways
    - `file` ignores the entire file
    - `line` ignores the order of the lines in the file

--- dmoj/checkers/sorted.py
+++ dmoj/checkers/sorted.py
@@ -1,16 +1,33 @@
-def check(process_output, judge_output, **kwargs):
+def check(process_output, judge_output, file=False,
+          line=False, line_sep='\n', token_sep=None, **kwargs):
+
+    if file:
+        import re
+        return  sorted(filter(None, re.split(token_sep), process_output)) ==
+                sorted(filter(None, re.split(token_sep), judge_output))
+
+    process_lines = filter(None, process_output.split(line_sep))
+    judge_lines = filter(None, judge_output.split(line_sep))
+
+    from functools import partial
     from itertools import izip
-    from string import split
-    token = kwargs.get('token', '\n')
-    process_tokens = filter(None, process_output.split(token))
-    judge_tokens = filter(None, judge_output.split(token))
-    if len(process_tokens) != len(judge_tokens):
+    import string
+
+    if len(process_lines) != len(judge_lines):
         return False
-    process_tokens = map(split, process_tokens)
-    judge_tokens = map(split, judge_tokens)
-    process_tokens.sort()
-    judge_tokens.sort()
-    for process_token, judge_token in izip(process_tokens, judge_tokens):
+
+    process_tokens = map(partial(string.split, sep=token_sep), process_lines)
+    judge_tokens = map(partial(string.split, sep=token_sep), judge_lines)
+
+    for i in xrange(len(judge_tokens)):
+        process_tokens[i].sort()
+        judge_tokens[i].sort()
+
+    if line:
+        process_lines.sort()
+        judge_lines.sort()
+
+    for process_token, judge_token in izip(process_lines, judge_lines):
         if process_token != judge_token:
             return False
     return True
